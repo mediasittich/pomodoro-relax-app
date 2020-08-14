ce43619525b6de41549e9fc853e0d53276c39a81
Regina Galambos <gina.galambos@gmail.com>
2020-08-14 12:52:39 +0200

    add initial components

---
+++ src/containers/Pomodoro.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PomodoroApp = () => {
+  return <div>Pomodoro Page</div>;
+};
+
+export default PomodoroApp;
---
+++ src/containers/Relax.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RelaxApp = () => {
+  return <div>Relax Page</div>;
+};
+
+export default RelaxApp;
