f59e2120369a0d8b1043eca7af43f19fe4b820aa
Regina Galambos <gina.galambos@gmail.com>
2020-08-15 21:19:20 +0200

    create component setup

--- src/containers/Pomodoro.js
+++ src/containers/Pomodoro.js
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import TimerInput from "../components/TimerInput";
+import ControlButton from "../components/ControlButton";
 
+import TimerDisplay from "../components/TimerDisplay";
+import DisplayReducingCircle from "../components/DisplayReducingCircle";
+
+import "./Pomodoro.css";
 const PomodoroApp = () => {
-  return <div>Pomodoro Page</div>;
+  const [minutes, setMinutes] = useState(1);
+  const [seconds, setSeconds] = useState(minutes * 60);
+  const [secondsLeft, setSecondsLeft] = useState(seconds);
+  // const [timeLeft, setTimeLeft] = useState(25 * 60);
+  const [isActive, setIsActive] = useState(false);
+
+  // Setting the Timer
+  const handleUp = () => {
+    setMinutes(minutes + 1 * 60);
+  };
+  const handleDown = () => {
+    setMinutes(minutes - 1 * 60);
+  };
+  // Control the Timer
+  const toggleTimer = () => {
+    setIsActive(!isActive);
+  };
+  const resetTimer = () => {
+    setIsActive(false);
+    setSecondsLeft(seconds);
+  };
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSecondsLeft((secondsLeft) => secondsLeft - 1);
+      }, 1000);
+    } else if (!isActive && secondsLeft !== 0) {
+      clearInterval(interval);
+    }
+    // console.log(seconds);
+    return () => clearInterval(interval);
+  }, [isActive, secondsLeft]);
+
+  return (
+    <div className="app pomodoro-app">
+      {/* <div>time left</div> */}
+      <div className="app-content">
+        <TimerInput
+          timeSet={seconds}
+          handleUp={handleUp}
+          handleDown={handleDown}
+        />
+        <TimerDisplay>
+          <DisplayReducingCircle time={seconds} timeremaining={secondsLeft} />
+        </TimerDisplay>
+        <ControlButton
+          title={isActive ? "pause" : "start"}
+          action={toggleTimer}
+        />
+        <ControlButton title="reset" action={resetTimer} />
+      </div>
+    </div>
+  );
 };
 
 export default PomodoroApp;
--- src/containers/Relax.js
+++ src/containers/Relax.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import TimerInput from "../components/TimerInput";
+import ControlButton from "../components/ControlButton";
+
+import TimerDisplay from "../components/TimerDisplay";
+import DisplaySplitCircle from "../components/DisplaySplitCircle";
 
 const RelaxApp = () => {
-  return <div>Relax Page</div>;
+  const totalTime = 7500;
+  const breatheTime = (totalTime / 5) * 2;
+  const holdTime = totalTime / 5;
+
+  return (
+    <div className="app relax-app">
+      {/* <div>time left</div> */}
+      <div className="app-content">
+        <TimerDisplay>
+          <DisplaySplitCircle
+            totalTime={totalTime}
+            breatheTime={breatheTime}
+            holdTime={holdTime}
+          />
+        </TimerDisplay>
+      </div>
+    </div>
+  );
 };
 
 export default RelaxApp;
