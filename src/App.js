26602acf89ab02f18fcc31e9e2c1baaf55bdf4ce
Regina Galambos <gina.galambos@gmail.com>
2020-08-16 13:04:38 +0200

    install bootstrap create  separate navigations for desktop & mobile

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  BrowserRouter,
+} from "react-router-dom";
 import Navbar from "./components/Navbar.js";
 import Home from "./components/Home.js";
 import PomodoroApp from "./containers/Pomodoro.js";
 import RelaxApp from "./containers/Relax.js";
 import MeditateApp from "./containers/Meditate.js";
 
-import "./App.css";
+// import "./App.css";
 
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
-        <div>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/pomodoro" component={PomodoroApp} />
-            <Route path="/relax" component={RelaxApp} />
-            <Route path="/meditate" component={MeditateApp} />
-          </Switch>
-        </div>
+        {/* <BrowserRouter> */}
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/pomodoro" component={PomodoroApp} />
+          <Route path="/relax" component={RelaxApp} />
+          <Route path="/meditate" component={MeditateApp} />
+        </Switch>
+        {/* </BrowserRouter> */}
       </div>
     </Router>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,85 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { Navbar, Container, Nav, NavItem } from "react-bootstrap";
+import { FiWind } from "react-icons/fi";
+import { RiHome2Line } from "react-icons/ri";
+import { GiMeditation } from "react-icons/gi";
+import { IoIosTimer } from "react-icons/io";
 
-const Navbar = () => {
+const tabs = [
+  {
+    id: 1,
+    exact: true,
+    route: "/",
+    label: "Home",
+    icon: <RiHome2Line />,
+  },
+  {
+    id: 2,
+    exact: false,
+    route: "/pomodoro",
+    label: "Pomodoro",
+    icon: <IoIosTimer />,
+  },
+  {
+    id: 3,
+    exact: false,
+    route: "/relax",
+    label: "Breathe",
+    icon: <FiWind />,
+  },
+  {
+    id: 4,
+    exact: false,
+    route: "/meditate",
+    label: "Meditate",
+    icon: <GiMeditation />,
+  },
+];
+
+const Navigation = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/pomodoro">Pomodoro</Link>
-        </li>
-        <li>
-          <Link to="/relax">Relax</Link>
-        </li>
-        <li>
-          <Link to="/meditate">Meditate</Link>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      {/* Top Navigation on large screens */}
+      <Navbar expand="lg" sticky="top" className="d-none d-lg-block">
+        <Container>
+          <Navbar.Brand>stuff</Navbar.Brand>
+          <Nav className="ml-auto">
+            {tabs.map((tab) => (
+              <NavItem key={tab.id}>
+                <NavLink
+                  exact={tab.exact}
+                  activeClassName="active"
+                  to={tab.route}
+                >
+                  {tab.label}
+                </NavLink>
+              </NavItem>
+            ))}
+          </Nav>
+        </Container>
+      </Navbar>
+      {/* Bottom Navigation on small screens */}
+      <Navbar expand fixed="bottom" className="d-block d-lg-none">
+        <Nav className="w-100 justify-content-around">
+          {tabs.map((tab) => (
+            <NavItem key={tab.id}>
+              <NavLink
+                exact={tab.exact}
+                activeClassName="active"
+                to={tab.route}
+              >
+                <div className="row flex-column justify-content-center align-items-center">
+                  {tab.icon}
+                  <div>{tab.label}</div>
+                </div>
+              </NavLink>
+            </NavItem>
+          ))}
+        </Nav>
+      </Navbar>
+    </div>
   );
 };
 
-export default Navbar;
+export default Navigation;
