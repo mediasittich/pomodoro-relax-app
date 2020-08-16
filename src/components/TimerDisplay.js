a3d406aedbf36ee779158af74cebfea47bb472fc
Regina Galambos <gina.galambos@gmail.com>
2020-08-17 01:40:14 +0200

    rm unused files

--- src/components/TimerDisplay.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./TimerDisplay.css";
-
-const TimerDisplay = ({ children }) => {
-  return <div className="display">{children}</div>;
-};
-
-export default TimerDisplay;
